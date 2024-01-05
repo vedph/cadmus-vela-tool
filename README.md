b1817241db50b6668ee1f0cabd5b50aae491b3ca
Myrmex <dfusi@hotmail.com>
2024-01-05 14:53:01 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Cadmus VeLA CLI Tool
+
+This is a command-line tool providing administrative functions for [Cadmus VeLA](https://github.com/vedph/cadmus-vela). Currently, it is designed to import data from Excel files into the VeLA database.
---
+++ vela-tool/Services/ConfigurationService.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Cadmus.Vela.Cli.Services;
+
+internal static class ConfigurationService
+{
+    private static IConfiguration? _configuration;
+
+    public static IConfiguration Configuration
+    {
+        get
+        {
+            _configuration ??= new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile("appsettings.local.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables()
+                .Build();
+
+            return _configuration;
+        }
+    }
+}
