cd76ef8a8198ac572fdcd5442ea287e80b2e69d4
Myrmex <dfusi@hotmail.com>
2024-01-28 14:56:54 +0100

    Added parser

---
+++ Cadmus.Vela.Import/ColFigEntryRegionParser.cs
@@ -0,0 +1,93 @@
+﻿using Cadmus.Import.Proteus;
+using Cadmus.Vela.Parts;
+using Fusi.Tools.Configuration;
+using Microsoft.Extensions.Logging;
+using Proteus.Core.Entries;
+using Proteus.Core.Regions;
+using System;
+using System.Collections.Generic;
+
+namespace Cadmus.Vela.Import;
+
+/// <summary>
+/// VeLA column tipo_figurativo entry region parser. This targets
+/// <see cref="GrfFramePart.Figure"/>.
+/// </summary>
+/// <seealso cref="EntryRegionParser" />
+/// <seealso cref="IEntryRegionParser" />
+[Tag("entry-region-parser.vela.col-tipo_figurativo")]
+public sealed class ColFigEntryRegionParser : EntryRegionParser,
+    IEntryRegionParser
+{
+    private readonly ILogger<ColFigEntryRegionParser>? _logger;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ColFigEntryRegionParser"/>
+    /// class.
+    /// </summary>
+    /// <param name="logger">The logger.</param>
+    public ColFigEntryRegionParser(
+        ILogger<ColFigEntryRegionParser>? logger = null)
+    {
+        _logger = logger;
+    }
+
+    /// <summary>
+    /// Determines whether this parser is applicable to the specified
+    /// region. Typically, the applicability is determined via a configurable
+    /// nested object, having parameters like region tag(s) and paths.
+    /// </summary>
+    /// <param name="set">The entries set.</param>
+    /// <param name="regions">The regions.</param>
+    /// <param name="regionIndex">Index of the region.</param>
+    /// <returns>
+    ///   <c>true</c> if applicable; otherwise, <c>false</c>.
+    /// </returns>
+    /// <exception cref="ArgumentNullException"></exception>
+    public bool IsApplicable(EntrySet set, IReadOnlyList<EntryRegion> regions,
+        int regionIndex)
+    {
+        ArgumentNullException.ThrowIfNull(set);
+        ArgumentNullException.ThrowIfNull(regions);
+
+        return regions[regionIndex].Tag == "col-tipo_figurativo";
+    }
+
+    /// <summary>
+    /// Parses the region of entries at <paramref name="regionIndex" />
+    /// in the specified <paramref name="regions" />.
+    /// </summary>
+    /// <param name="set">The entries set.</param>
+    /// <param name="regions">The regions.</param>
+    /// <param name="regionIndex">Index of the region in the set.</param>
+    /// <returns>
+    /// The index to the next region to be parsed.
+    /// </returns>
+    /// <exception cref="ArgumentNullException">set or regions</exception>
+    public int Parse(EntrySet set, IReadOnlyList<EntryRegion> regions,
+        int regionIndex)
+    {
+        ArgumentNullException.ThrowIfNull(set);
+        ArgumentNullException.ThrowIfNull(regions);
+
+        CadmusEntrySetContext ctx = (CadmusEntrySetContext)set.Context;
+        EntryRegion region = regions[regionIndex];
+
+        if (ctx.CurrentItem == null)
+        {
+            _logger?.LogError("tipo_figurativo column without any item " +
+                "at region {region}", region);
+            throw new InvalidOperationException(
+                "tipo_figurativo column without any item at region " + region);
+        }
+
+        DecodedTextEntry txt = (DecodedTextEntry)
+            set.Entries[region.Range.Start.Entry + 1];
+        string? value = VelaHelper.FilterValue(txt.Value, false);
+
+        GrfFramePart part = ctx.EnsurePartForCurrentItem<GrfFramePart>();
+        part.Figure = value;
+
+        return regionIndex + 1;
+    }
+}
