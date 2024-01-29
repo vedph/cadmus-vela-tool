1a426787bfd9b9dc00cacd93cd904cb3a308620f
Myrmex <dfusi@hotmail.com>
2024-01-29 11:34:50 +0100

    Comment

---
+++ Cadmus.Vela.Import/ColCommentEntryRegionParser.cs
@@ -0,0 +1,95 @@
+﻿using Cadmus.General.Parts;
+using Cadmus.Import.Proteus;
+using Fusi.Tools.Configuration;
+using Microsoft.Extensions.Logging;
+using Proteus.Core.Entries;
+using Proteus.Core.Regions;
+using System;
+using System.Collections.Generic;
+
+namespace Cadmus.Vela.Import;
+
+/// <summary>
+/// VeLA column commento entry region parser. This targets <see cref="NotePart"/>.
+/// </summary>
+/// <seealso cref="EntryRegionParser" />
+/// <seealso cref="IEntryRegionParser" />
+[Tag("entry-region-parser.vela.col-commento")]
+public sealed class ColCommentEntryRegionParser : EntryRegionParser,
+    IEntryRegionParser
+{
+    private readonly ILogger<ColCommentEntryRegionParser>? _logger;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ColCommentEntryRegionParser"/>
+    /// class.
+    /// </summary>
+    /// <param name="logger">The logger.</param>
+    public ColCommentEntryRegionParser(
+        ILogger<ColCommentEntryRegionParser>? logger = null)
+    {
+        _logger = logger;
+    }
+
+    /// <summary>
+    /// Determines whether this parser is applicable to the specified
+    /// region. Typically, the applicability is determined via a configurable
+    /// nested object, having parameters like region tag(s) and paths.
+    /// </summary>
+    /// <param name="set">The entries set.</param>
+    /// <param name="regions">The regions.</param>
+    /// <param name="regionIndex">Index of the region.</param>
+    /// <returns>
+    ///   <c>true</c> if applicable; otherwise, <c>false</c>.
+    /// </returns>
+    /// <exception cref="ArgumentNullException"></exception>
+    public bool IsApplicable(EntrySet set, IReadOnlyList<EntryRegion> regions,
+        int regionIndex)
+    {
+        ArgumentNullException.ThrowIfNull(set);
+        ArgumentNullException.ThrowIfNull(regions);
+
+        return regions[regionIndex].Tag == "col-commento";
+    }
+
+    /// <summary>
+    /// Parses the region of entries at <paramref name="regionIndex" />
+    /// in the specified <paramref name="regions" />.
+    /// </summary>
+    /// <param name="set">The entries set.</param>
+    /// <param name="regions">The regions.</param>
+    /// <param name="regionIndex">Index of the region in the set.</param>
+    /// <returns>
+    /// The index to the next region to be parsed.
+    /// </returns>
+    /// <exception cref="ArgumentNullException">set or regions</exception>
+    public int Parse(EntrySet set, IReadOnlyList<EntryRegion> regions,
+        int regionIndex)
+    {
+        ArgumentNullException.ThrowIfNull(set);
+        ArgumentNullException.ThrowIfNull(regions);
+
+        CadmusEntrySetContext ctx = (CadmusEntrySetContext)set.Context;
+        EntryRegion region = regions[regionIndex];
+
+        if (ctx.CurrentItem == null)
+        {
+            _logger?.LogError("commento column without any item at region {region}",
+                region);
+            throw new InvalidOperationException(
+                "commento column without any item at region " + region);
+        }
+
+        DecodedTextEntry txt = (DecodedTextEntry)
+            set.Entries[region.Range.Start.Entry + 1];
+        string? value = VelaHelper.FilterValue(txt.Value, false);
+
+        if (!string.IsNullOrEmpty(value))
+        {
+            NotePart part = ctx.EnsurePartForCurrentItem<NotePart>();
+            part.Text = value;
+        }
+
+        return regionIndex + 1;
+    }
+}
