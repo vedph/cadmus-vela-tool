dc04eda07517b664ba5b5106cddbbb4c67497546
Myrmex <dfusi@hotmail.com>
2024-11-04 15:24:52 +0100

    Create ColEditionEntryRegionParser.cs

---
+++ Cadmus.Vela.Import/ColEditionEntryRegionParser.cs
@@ -0,0 +1,115 @@
+﻿using Cadmus.Import.Proteus;
+using Cadmus.General.Parts;
+using Fusi.Tools.Configuration;
+using Microsoft.Extensions.Logging;
+using Proteus.Core.Entries;
+using Proteus.Core.Regions;
+using System;
+using System.Collections.Generic;
+
+namespace Cadmus.Vela.Import;
+
+/// <summary>
+/// VeLA column edizione e commento children entris region parser. This targets
+/// <see cref="MetadataPart"/> for placeholders to be later restructured into
+/// their specific parts.
+/// </summary>
+/// <seealso cref="EntryRegionParser" />
+/// <seealso cref="IEntryRegionParser" />
+[Tag("entry-region-parser.vela.col-edizione")]
+public sealed class ColEditionEntryRegionParser(
+    ILogger<ColEditionEntryRegionParser>? logger = null) : EntryRegionParser,
+    IEntryRegionParser
+{
+    private const string COL_EDIZIONE = "col-edizione";
+    private const string COL_COMMENTO = "col-commento";
+    private const string COL_BIBLIOGRAFIA = "col-bibliografia";
+    private readonly ILogger<ColEditionEntryRegionParser>? _logger = logger;
+
+    /// <summary>
+    /// Determines whether this parser is applicable to the specified
+    /// region. Typically, the applicability is determined via a configurable
+    /// nested object, having parameters like region tag(s) and paths.
+    /// </summary>
+    /// <param name="set">The entries set.</param>
+    /// <param name="regions">The regions.</param>
+    /// <param name="regionIndex">Index of the region.</param>
+    /// <returns>
+    ///   <c>true</c> if applicable; otherwise, <c>false</c>.
+    /// </returns>
+    /// <exception cref="ArgumentNullException"></exception>
+    public bool IsApplicable(EntrySet set, IReadOnlyList<EntryRegion> regions,
+        int regionIndex)
+    {
+        ArgumentNullException.ThrowIfNull(set);
+        ArgumentNullException.ThrowIfNull(regions);
+
+        return regions[regionIndex].Tag == COL_EDIZIONE ||
+            regions[regionIndex].Tag == COL_COMMENTO ||
+            regions[regionIndex].Tag == COL_BIBLIOGRAFIA;
+    }
+
+    /// <summary>
+    /// Parses the region of entries at <paramref name="regionIndex" />
+    /// in the specified <paramref name="regions" />.
+    /// </summary>
+    /// <param name="set">The entries set.</param>
+    /// <param name="regions">The regions.</param>
+    /// <param name="regionIndex">Index of the region in the set.</param>
+    /// <returns>
+    /// The index to the next region to be parsed.
+    /// </returns>
+    /// <exception cref="ArgumentNullException">set or regions</exception>
+    public int Parse(EntrySet set, IReadOnlyList<EntryRegion> regions,
+        int regionIndex)
+    {
+        ArgumentNullException.ThrowIfNull(set);
+        ArgumentNullException.ThrowIfNull(regions);
+
+        CadmusEntrySetContext ctx = (CadmusEntrySetContext)set.Context;
+        EntryRegion region = regions[regionIndex];
+
+        if (ctx.CurrentItem == null)
+        {
+            _logger?.LogError("{Tag} column without any item at region {Region}",
+                region.Tag, region);
+            throw new InvalidOperationException(
+                $"{region.Tag} column without any item at region " + region);
+        }
+
+        DecodedTextEntry txt = (DecodedTextEntry)
+            set.Entries[region.Range.Start.Entry + 1];
+        string? value = VelaHelper.FilterValue(txt.Value, false);
+        if (!string.IsNullOrEmpty(value))
+        {
+            MetadataPart part = ctx.EnsurePartForCurrentItem<MetadataPart>();
+
+            switch (region.Tag)
+            {
+                case COL_EDIZIONE:
+                    part.Metadata.Add(new Metadatum
+                    {
+                        Name = "_edition",
+                        Value = value
+                    });
+                    break;
+                case COL_COMMENTO:
+                    part.Metadata.Add(new Metadatum
+                    {
+                        Name = "_comment",
+                        Value = value
+                    });
+                    break;
+                case COL_BIBLIOGRAFIA:
+                    part.Metadata.Add(new Metadatum
+                    {
+                        Name = "_biblio",
+                        Value = value
+                    });
+                    break;
+            }
+        }
+
+        return regionIndex + 1;
+    }
+}
